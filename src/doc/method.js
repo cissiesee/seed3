003e68135a68b0321ae354c8c16a1e2cd47fd8ad
huangzheting <huangzheting@zhihuiya.com>
2015-06-14 23:27:57 +0800

    doc(api) for seed3

---
+++ src/doc/method.js
@@ -0,0 +1,28 @@
+//instance method
+
+[
+	{
+		'name': 'setOption', 'description': '', 'type': 'function', 'return': '', 'props': '#options'
+	},
+	{
+		'name': 'getOption'
+	},
+	{
+		'name': 'setTheme'
+	},
+	{
+		'name': 'getTheme'
+	},
+	{
+		'name': 'resize'
+	},
+	{
+		'name': 'destroy'
+	},
+	{
+		'name': 'getDom'
+	},
+	{
+		'name': 'setDom'
+	},
+]
---
+++ src/doc/start.js
@@ -0,0 +1,3 @@
+var Seed3 = require('seed3');
+var chartInstance = new Seed3(config); //without dom
+chartInstance.setDom(document.getElementByTag('body')[0]);
